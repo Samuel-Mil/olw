2aac5a5e07a33753c3dfa5333625c14e46d6804e
Samuel-Mil <samuel.mil.mar@gmail.com>
2023-02-10 16:16:43 -0300

    Clients CRUD and OpenAI integration

--- app/Models/SalesCommission.php
+++ app/Models/SalesCommission.php
@@ -9,4 +9,19 @@ class SalesCommission extends Model
     protected $table = "sales_commission_view";
     public $incrementing = false;
     public $timestamps = false;
+
+    public function scopeGetColumns()
+    {
+        return [
+            'company',
+            'seller',
+            'client',
+            'city',
+            'state',
+            'sold_at',
+            'status',
+            'total_amount',
+            'commission'
+        ];
+    }
 }
